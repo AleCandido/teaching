d770f499b0ea64dda39eaa66e9cce0b08e683f80
Alessandro Candido <candido.ale@gmail.com>
2021-09-16 22:21:38 +0200

    Revert stupid link faking

--- src/lib/Card.svelte
+++ src/lib/Card.svelte
@@ -1,28 +1,25 @@
 <script>
 	export let year;
 	export let details;
-    
-    function navigate(e) {
-        location.href=`/courses/${year}/${details.project}`;
-    }
 </script>
 
-<article on:click={navigate} class="w-96 m-5 h-40 flex border border-gray-500 rounded-lg overflow-hidden bg-white">
-    <img src="/books-small.jpg" alt="" class="flex-grow-0 h-full w-2/5">
-    <div class="w-7/12 pt-2 mr-3 ml-3">
-        <h4 class="mb-2 capitalize font-bold underline">
-            {details.title}
-        </h4>
-        <p class="text-sm pl-1">
-            {details.description}
-        </p>
-    </div>
-</article>
+<a href="/courses/{year}/{details.project}">
+    <article class="w-96 m-5 h-40 flex border border-gray-500 rounded-lg overflow-hidden bg-white">
+        <img src="/books-small.jpg" alt="" class="flex-grow-0 h-full w-2/5">
+        <div class="w-7/12 pt-2 mr-3 ml-3">
+            <h4 class="mb-2 capitalize font-bold underline">
+                {details.title}
+            </h4>
+            <p class="text-sm pl-1">
+                {details.description}
+            </p>
+        </div>
+    </article>
+</a>
 
 <style>
 	article {
 		box-shadow: 3px 3px black;
-        cursor: pointer;
 	}
 
 	article:hover {
